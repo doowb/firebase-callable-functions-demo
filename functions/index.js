2aab792533a3f9a4ef694e61231e093c219c7f26
doowb <brian.woodward@gmail.com>
2019-05-17 15:18:57 -0400

    output GCLOUD_PROJECT in demo function for debugging

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,6 @@ const functions = require('firebase-functions');
 
 let value = 0;
 exports.increment = functions.https.onCall(async (data, context) => {
+  console.log(process.env.GCLOUD_PROJECT);
   return (++value);
 });
